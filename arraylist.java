34bf0c2e2775733ff3907dafdb7de37e3bc0a611
Mohit Singh <mohitsingh.it5@gmail.com>
2020-03-24 20:36:52 +0530

    basic methods

--- arraylist.java
+++ arraylist.java
@@ -9,7 +9,12 @@ public class arraylist {
         List<Integer> intlist = new ArrayList<Integer>();
         intlist.add(1);
         intlist.add(2);
-        intlist.add(1);
+        intlist.add(100);
+        intlist.add(100);
+        intlist.add(100);
+        intlist.add(500);
+        intlist.add(800);
+        intlist.add(500);
         List<Integer> linkedlist = new LinkedList<>();
         linkedlist.add(5);
         linkedlist.add(6);
@@ -21,11 +26,38 @@ public class arraylist {
         // so as to make the type conversion easy
         intlist.addAll(linkedlist);
         intlist.add(1, 8); // adding 8 to index 1
-
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(100);
+        arr.add(500);
+        arr.add(800);
         // for each loop ==>
 
         for (int i : intlist) {
             System.out.println(i);
         }
+
+        // other basic functions
+        System.out.println("get at pos 2 : " + intlist.get(2));
+        System.out.println("isEmpty " + intlist.isEmpty());
+        System.out.println("contains return boolean " + intlist.contains(1));
+        System.out.println("remove element at index 2 " + intlist.remove(2));
+        System.out.println("size (to get length) " + intlist.size());
+        System.out.println("removeAll to remove iterables " + intlist.removeAll(arr));
+        // converting to array
+        // by default toArray gives array of Object data type
+        Object ob[] = intlist.toArray();
+        System.out.println("Object array : =");
+        for (Object i : ob) {
+            System.out.println(i);
+        }
+        // to get the array in desired dataype we pass the object of that data type as
+        // parameter after creating the object of that data type
+        Integer obj[] = new Integer[intlist.size()];
+        intlist.toArray(obj);
+        System.out.println("Ineteger Array");
+        for (Integer i : obj) {
+            System.out.println(i);
+        }
+
     }
 }
